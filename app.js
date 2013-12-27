0787d27b78bbe65e1deb091ce489eed6cfa65e13
Taron Foxworth <taronfoxworth@gmail.com>
2013-12-27 13:48:27 -0600

    Cleaning up

--- app.js
+++ app.js
@@ -4,8 +4,7 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var pushover = require('./routes/pushover');
+var pushover = require('./controllers/pushover');
 var http = require('http');
 var path = require('path');
 
@@ -13,25 +12,13 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(express.bodyParser());
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
 app.post('/', pushover.send);
 
 http.createServer(app).listen(app.get('port'), function(){
